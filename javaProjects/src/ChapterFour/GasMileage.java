08880f6112b55286999d8cf3f689a73b324bf7d2
Akaabiam Richards <richardsakaabiam@gmail.com>
2022-12-22 11:02:37 +0100

    updated

---
+++ javaProjects/src/ChapterFour/GasMileage.java
@@ -0,0 +1,34 @@
+package ChapterFour;
+
+import java.util.Scanner;
+
+public class GasMileage {
+    int totalMilesDriven;
+    int totalGallonUsed;
+    int milesDrivenCounter;
+    int gallonCounter;
+   Scanner input = new Scanner(System.in);
+   public void milesDriven(){
+       int milesDriven = 0;
+       while(milesDriven != -1){
+           System.out.println("Enter miles covered");
+            milesDriven = input.nextInt();
+            totalMilesDriven = totalMilesDriven + milesDriven;
+           milesDrivenCounter +=   1;
+       }
+   }
+
+   public void gallonUsed(){
+       int gallonUsed = 0;
+       while (gallonUsed !=-1){
+           System.out.println("Enter gallon used");
+           gallonUsed = input.nextInt();
+           totalGallonUsed = totalGallonUsed + gallonUsed;
+           gallonCounter +=  1;
+       }
+   }
+   public void averageMilesDrivenPerGallon(){
+       double averageMilesDrivenPerGallon =(double) (totalMilesDriven)/(totalGallonUsed);
+       System.out.printf("The average miles driven per gallon is: %f%n",averageMilesDrivenPerGallon);
+   }
+}
---
+++ javaProjects/src/ChapterFour/GasMileageMain.java
@@ -0,0 +1,10 @@
+package ChapterFour;
+
+public class GasMileageMain {
+    public static void main(String[] args) {
+      GasMileage gasMileage = new GasMileage();
+        gasMileage.gallonUsed();
+        gasMileage.milesDriven();
+        gasMileage.averageMilesDrivenPerGallon();
+    }
+}
