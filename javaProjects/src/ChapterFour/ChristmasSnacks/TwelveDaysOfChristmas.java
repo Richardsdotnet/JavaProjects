d222fbd599406709a52dd8947d9159568c35ae52
Akaabiam Richards <richardsakaabiam@gmail.com>
2022-12-29 19:27:22 +0100

    updated

---
+++ javaProjects/src/ChapterFour/ChristmasSnacks/TwelveDaysOfChristmas.java
@@ -0,0 +1,93 @@
+package ChapterFour.ChristmasSnacks;
+
+public class TwelveDaysOfChristmas {
+    public  static void main(String[] args){
+        for (int days = 0; days < 12; days++){
+            switch (days){
+                case 1:
+                    System.out.println("On the first day of Christmas my true love gave to me\n" +
+                            "A partridge in a pear tree");
+                    System.out.println();
+                case 2:
+                    System.out.println("On the second day of Christmas my true love gave to me\n" +
+                            "Two turtle doves and a partridge in a pear tree");
+                    System.out.println();
+                case 3:
+                    System.out.println("On the third day of Christmas my true love gave to me\n" +
+                            "Three French hens, two turtle doves and a partridge in a pear tree");
+                    System.out.println();
+                case 4:
+                    System.out.println("""
+                            On the fourth day of Christmas my true love gave to me
+                            Four calling birds, three French hens
+                            Two turtle doves and a partridge in a pear tree""");
+                    System.out.println();
+                case 5:
+                    System.out.println("""
+                            On the fifth day of Christmas my true love gave to me
+                            Five gold rings, four calling birds, three French hens
+                            Two turtle doves and a partridge in a pear tree""");
+                    System.out.println();
+                case 6:
+                    System.out.println("""
+                            On the sixth day of Christmas my true love gave to me
+                            Six geese a laying, five gold rings
+                            Four calling birds, three French hens
+                            Two turtle doves and a partridge in a pear tree""");
+                    System.out.println();
+                case 7:
+                    System.out.println("""
+                            On the seventh day of Christmas my true love gave to me
+                            Seven swans a swimming, six geese a laying, five gold rings
+                            Four calling birds, three French hens
+                            Two turtle doves and a partridge in a pear tree""");
+                    System.out.println();
+                case 8:
+                    System.out.println("""
+                            On the eighth day of Christmas my true love gave to me
+                            Eight maids a milking, seven swans a swimming
+                            Six geese a laying, five gold rings
+                            Four calling birds, three French hens
+                            Two turtle doves and a partridge in a pear tree""");
+                    System.out.println();
+                case 9:
+                    System.out.println("""
+                            On the ninth day of Christmas
+                            Nine ladies dancing, eight maids a milking
+                            Seven swans a swimming, six geese a laying, five gold rings
+                            Four calling birds, three French hens
+                            Two turtle doves and a partridge in a pear tree""");
+                    System.out.println();
+                case 10:
+                    System.out.println("""
+                            On the tenth day of Christmas my true love gave to me
+                            Ten lords a leaping, nine ladies dancing, eight maids a milking
+                            Seven swans a swimming, six geese a laying, five gold rings
+                            Four calling birds, three French hens
+                            Two turtle doves and a partridge in a pear tree""");
+                    System.out.println();
+                case 11:
+                    System.out.println("""
+                            On the eleventh day of Christmas my true love gave to me
+                            Eleven pipers piping, ten lords a leaping
+                            Nine ladies dancing, eight maids a milking
+                            Seven swans a swimming, six geese a laying, five gold rings
+                            Four calling birds, three French hens
+                            Two turtle doves and a partridge in a pear tree""");
+                    System.out.println();
+                case 12:
+                    System.out.println("""
+                            On the twelfth day of Christmas my true love gave to me
+                            Twelve drummers drumming, eleven pipers piping
+                            Ten lords a leaping, nine ladies dancing, eight maids a milking
+                            Seven swans a swimming, six geese a laying, five gold rings
+                            Four calling birds, three French hens
+                            Two turtle doves and a partridge in a pear tree""");
+                    System.out.println();
+                    break;
+                default:
+            }
+            System.out.println("Merry Christmas");
+        }
+    }
+}
